03bb808fe8e9ff94c407714c71c0ff2614e16c08
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-04-26 14:54:46 -0700

    testing works

--- javascript/data-structures/linkedList/__test__/linked-list.test.js
+++ javascript/data-structures/linkedList/__test__/linked-list.test.js
@@ -1,25 +1,29 @@
 'use strict';
 
-const LinkedList = require('../lib/ll.js');
+// const LinkedList = require('../lib/ll.js');
 const LL = require('../lib/ll.js');
 
 describe("Linked List Test", () => {
     it('should create an empty linked list on instantiation', () => {
         let list = new LL();
-        expect(list.head).toEqual(null)
+        expect(list.head).toEqual(null);
 
     })
 
-    it('should add a node to the list', () => {
+    it('should add a node to the list (append + more than 1)', () => {
         let list = new LL();
         let first = 1;
         let second = 2;
+        let third = 3;
 
         list.append(first);
         expect(list.head.value).toEqual(1);
 
         list.append(second);
-        expect(list.head.next.value). toEqual(2)
+        expect(list.head.next.value). toEqual(2);
+
+        list.append(third);
+        expect(list.head.next.next.value).toEqual(3);
         
     })
 
@@ -32,7 +36,39 @@ describe("Linked List Test", () => {
         expect(list.head.value).toEqual(1);
 
         list.insert(second);
-        expect(list.head.value). toEqual(2)
+        expect(list.head.value). toEqual(2);
+        
+    })
+
+    it('Can properly insert new node into position before first instance of value, into the linked list', () => {
+        let list = new LL();
+        let first = 1;
+        let second = 2;
+        let third = 3;
+        let fourth = 4;
+
+        list.append(first);
+        list.append(second);
+        list.append(fourth);
+
+        list.insertBefore(4, third);
+        expect(list.head.next.next.value).toEqual(3);
+        
+    })
+
+    it('Can properly insert new node into position after first instance of value, into the linked list', () => {
+        let list = new LL();
+        let first = 1;
+        let second = 2;
+        let third = 3
+        let fourth = 4
+
+        list.append(first);
+        list.append(second);
+        list.append(fourth);
+
+        list.insertAfter(2, third);
+        expect(list.head.next.next.value).toEqual(3);
         
     })
 
@@ -45,7 +81,7 @@ describe("Linked List Test", () => {
         expect(list.head.value).toEqual(1);
 
         list.append(second);
-        expect(list.head.value). toEqual(1)
+        expect(list.head.value).toEqual(1);
         
     })
 
@@ -58,8 +94,8 @@ describe("Linked List Test", () => {
         list.append(first);
         list.append(second);
         list.append(third);
-        let result = list.includes(3)
-        expect(result).toEqual(false)
+        let result = list.includes(3);
+        expect(result).toEqual(false);
 
     })
 
@@ -70,8 +106,8 @@ describe("Linked List Test", () => {
 
         list.append(first);
         list.append(second);
-        let result = list.includes(3)
-        expect(result).toEqual(false)
+        let result = list.includes(3);
+        expect(result).toEqual(false);
 
     })
 
@@ -83,10 +119,21 @@ describe("Linked List Test", () => {
         list.append(first);
         list.append(second);
         list.append(fourth);
-
         let testing = list.toString();
-        console.log("Testing: ", testing);
         expect(testing).toEqual('{ 1 } -> { 2 } -> { 44 } -> NULL');
+    })
+
+    it('Can properly delete a node of a given value', () => {
+        let list = new LL();
+        let first = 1;
+        let second = 2;
+        let fourth = 44;
+        list.append(first);
+        list.append(second);
+        list.append(fourth);
+
+        list.delete(2);
+        expect(list.head.next.value).toEqual(44);
     })
 
 })
