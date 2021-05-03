4d41d5eccf0fe85a10db8a051ececb19f6d5aeca
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-05-03 13:32:48 -0700

    e

---
+++ challenges/queueWithStacks/lib/stack.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Node = require('./node.js')
+
+
+class Stack {
+    constructor(){
+        this.length = 0;
+    }
+
+    push(value){
+        this[this.length] = value;
+        this.length++
+        return this
+    }
+
+    pop(){
+    if (this.length===0){return 'empty stack'}
+    let result = this[--this.length];
+    delete this[this.length]
+    return result
+    }
+
+    peek(){
+        return this[this.length-1]
+    }
+
+    isEmpty(){
+        return this.length === 0
+    }
+}
+
+module.exports = Stack;
+
+
+// =========
+
+// function Stack() {
+//     this.length = 0
+// }
+
+// Stack.prototype.push = function (val){
+//     this[this.length++] = val
+//     return this
+// }
+
+// Stack.prototype.pop = function (val){
+//     if (this.length===0){return 'empty stack'}
+//     let result = this[--this.length];
+//     delete[this.length]
+// }
+
+// Stack.prototype.peek = function (val){
+    //     return this[this.length-1]
+    // }
+
+    // Stack.prototype.isEmpty = function (val){
+    //     return this.length === 0
+    // }
